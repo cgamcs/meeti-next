9611ec5f8c464601056bb2801f89a73aa5e79df8
César García <cesar.garciamcs@proton.me>
2026-06-02 16:47:13 +0000

    creando barrel de exportaciones para componentes de formulario

---
+++ src/shared/components/forms/index.ts
@@ -0,0 +1,11 @@
+import Form from "./Form"
+import FormLabel from "./FormLabel"
+import FormInput from "./FormInput"
+import FormSubmit from "./FormSubmit"
+
+export {
+  Form,
+  FormLabel,
+  FormInput,
+  FormSubmit
+}
