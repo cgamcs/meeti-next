53b1520d26d4f74d7b95023dc1ad156ecf03b35e
César García <cesar.garciamcs@proton.me>
2026-06-02 17:45:30 +0000

    creando schema base y schema para el registro

---
+++ src/features/auth/schemas/authSchema.ts
@@ -0,0 +1,15 @@
+import z from "zod"
+
+export const BaseAuthSchema = z.object({
+  name: z.string().min(1, {error: "El nombre es obligatorio"}),
+  email: z.email({error: "El email es obligatorio"}),
+  password: z.string().min(8, {error: "La contraseña debe ser mínimo de 8 caracters"}),
+  passwordConfirmation: z.string().min(1, {error: "La contraseña de confirmación no debe de ir vacío"})
+})
+
+export const SignUpSchema = BaseAuthSchema.pick({
+  name: true,
+  email: true, 
+  password: true,
+  passwordConfirmation: true
+})
