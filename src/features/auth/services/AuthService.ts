71096a076d8f170bea284520504594a9ee60640e
César García <cesar.garciamcs@proton.me>
2026-06-03 16:46:12 +0000

    inyectando el repository en el service para una arquitectura desacoplada

--- src/features/auth/services/AuthService.ts
+++ src/features/auth/services/AuthService.ts
@@ -1,11 +1,17 @@
 import { auth } from "@/src/lib/auth"
 import { SignUpInput } from "../schemas/authSchema"
+import { authRepository, IAuthRepository } from "./AuthRepository"
 
 class AuthService {
+  constructor(
+    private authRepository: IAuthRepository
+  ){}
+
   async register(credentials: SignUpInput) {
     const { name, email, password } = credentials
 
     // Revisar si el usuario existe
+    await this.authRepository.userExists(email)
 
     // Validación de negocio
 
@@ -25,4 +31,4 @@ class AuthService {
   }
 }
 
-export const authService = new AuthService()
+export const authService = new AuthService(authRepository)
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -13,3 +13,4 @@ export const auth = betterAuth({
   },
   plugins: [nextCookies()]
 })
+
