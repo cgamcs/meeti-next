31e02d4f28aa286a5f35a3c30e6a81d007d4cea1
César García <cesar.garciamcs@proton.me>
2026-06-03 14:30:44 +0000

    actualizando drizzle a la version beta de su v1 y creando relaciones para better-auth

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,6 @@
-import { drizzle } from 'drizzle-orm/neon-http'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { relations } from './relations'
 
 export const db = drizzle(process.env.DATABASE_URL!, {
-  
+  relations
 })
--- src/db/schema/auth-schema.ts
+++ src/db/schema/auth-schema.ts
@@ -1,4 +1,3 @@
-import { relations } from "drizzle-orm";
 import { pgTable, text, timestamp, boolean, index } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
