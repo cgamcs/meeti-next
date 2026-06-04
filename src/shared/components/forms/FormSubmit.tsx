dfa32443f5d3385f1e43c9ff6c13bf152031e246
César García <cesar.garciamcs@proton.me>
2026-06-04 17:03:14 +0000

    Cambiando diseño de la fuente

--- src/shared/components/forms/FormSubmit.tsx
+++ src/shared/components/forms/FormSubmit.tsx
@@ -7,7 +7,7 @@ export default function FormSubmit(props: FormSubmittProps) {
     <input
       {...props}
       type="submit"
-      className="bg-pink-600 text-white font-black p-2 w-full cursor-pointer mt-3 rounded-lg hover:bg-pink-700/80"  
+      className="bg-pink-600 text-white font-bold p-2 w-full uppercase cursor-pointer mt-3 rounded-lg hover:bg-pink-700/80"  
     />
   )
 }
