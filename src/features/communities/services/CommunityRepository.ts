3eafc7bfd70abd821e940f64d56797bcbb6dc558
César García <cesar.garciamcs@proton.me>
2026-06-04 21:26:58 +0000

    creando el repositorio

--- src/features/communities/services/CommunityRepository.ts
+++ src/features/communities/services/CommunityRepository.ts
@@ -1,10 +1,15 @@
-export interface ICommunityRepository {
+import { db } from "@/src/db"
+import { InsertCommunity, SelectCommunity } from "../types/community.types"
+import { community } from "@/src/db/schema"
 
+export interface ICommunityRepository {
+  create(data: InsertCommunity) : Promise<SelectCommunity>
 }
 
 class CommunityRepository implements ICommunityRepository {
-  create() {
-    
+  async create(data: InsertCommunity) {
+    const [result] = await db.insert(community).values(data).returning()
+    return result
   }
 }
 
