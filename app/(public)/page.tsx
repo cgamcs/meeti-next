073b11378f02cf980ca41283953d915e451dc540
César García <cesar.garciamcs@proton.me>
2026-06-04 15:14:11 +0000

    creando funcion reutilizable para revisar si un usuario ya esta autenticado

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,18 +1,12 @@
 import { Metadata } from "next"
 import Hero from "@/components/ui/Hero"
 import { generatePageTitle } from "@/utils/metadata"
-import { auth } from "@/src/lib/auth"
-import { headers } from "next/headers"
 
 export const metadata : Metadata = {
   title: generatePageTitle("Inicio")
 }
 
 export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: await headers()
-  })
-
   return (
     <>
       <Hero />
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,14 @@
+import Heading from "@/components/typography/Heading"
+import { requireAuth } from "@/src/lib/auth-server"
+import { redirect } from "next/navigation"
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const { isAuth } = await requireAuth()
+  if (!isAuth) redirect('/auth/login')
+
   return (
-    <div>DashboardPage</div>
+    <>
+      <Heading>Panel de Administración</Heading>
+    </>
   )
 }
