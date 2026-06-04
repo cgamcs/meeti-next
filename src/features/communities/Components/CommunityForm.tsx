ee42b006e2833cf689a1124b61d347460e356283
César García <cesar.garciamcs@proton.me>
2026-06-04 21:57:21 +0000

    configurando uploadthing y obteniendo su api key

--- src/features/communities/Components/CommunityForm.tsx
+++ src/features/communities/Components/CommunityForm.tsx
@@ -2,6 +2,7 @@ import { FormError, FormInput, FormLabel } from "@/src/shared/components/forms"
 import FormTextArea from "@/src/shared/components/forms/FormTextArea"
 import { useFormContext } from "react-hook-form"
 import { CommunityInput } from "../schemas/communitySchema"
+import { UploadDropzone } from "@/src/shared/utils/uploadthing"
 
 export default function CommunityForm() {
   const { register, formState: { errors } } = useFormContext<CommunityInput>()
@@ -16,6 +17,8 @@ export default function CommunityForm() {
         {...register('name')}
       />
       {errors.name && <FormError>{errors.name.message}</FormError>}
+
+      <UploadDropzone />
 
       <FormLabel
         htmlFor="description">Descripción Comunidad</FormLabel>
