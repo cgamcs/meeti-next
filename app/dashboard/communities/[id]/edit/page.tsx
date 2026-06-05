8ebf071bfa746c4391fdb3021f423071fb264c9d
César García <cesar.garciamcs@proton.me>
2026-06-05 21:10:51 +0000

    obteniendo los detalles de la comunidad y protegiendo la pantalla segun los permisos del usuario

--- app/dashboard/communities/[id]/edit/page.tsx
+++ app/dashboard/communities/[id]/edit/page.tsx
@@ -1,15 +1,15 @@
 import { communityService } from '@/src/features/communities/services/CommunityService'
 import { requireAuth } from '@/src/lib/auth-server'
 import Heading from '@/src/shared/components/typography/Heading'
-import { redirect } from 'next/navigation'
+import { notFound, redirect } from 'next/navigation'
 
 export default async function EditCommunityPage(props: PageProps<'/dashboard/communities/[id]/edit'>) {
   const { session } = await requireAuth()
   if (!session) redirect('/')
 
   const { id } = await props.params
-  const community = await communityService.getCommunity(id)
-  console.log(community)
+  const community = await communityService.getCommunityDetails(id, session.user)
+  if (!community.permissions.canEdit) notFound()
 
   return (
     <>
--- src/features/communities/services/CommunityService.ts
+++ src/features/communities/services/CommunityService.ts
@@ -45,6 +45,24 @@ class CommunityService {
     if (!community) notFound()
     return community
   }
+
+  async getCommunityDetails(communityId: string, user: User) {
+    const community = await this.getCommunity(communityId)
+    const isMember = false
+    const isAdmin = CommunityPolicy.isAdmin(user, community)
+
+    return {
+      data: community,
+      context: {isMember, isAdmin},
+      permissions: {
+        canEdit: CommunityPolicy.canEdit(user, community),
+        canDelete: CommunityPolicy.canDelete(user, community),
+        canJoin: MembershipPolicy.canJoin(user, community, isMember),
+        canLeave: MembershipPolicy.canLeave(user, community, isMember),
+        canViewMembers: CommunityPolicy.canViewMembers(user, community)
+      }
+    }
+  }
 }
 
 export const communityService = new CommunityService(communityRepository)
